0e3555f2294aa7d6dd8f3f86d6c216dfdea9d36f
Jason Collum <jasoncollum@gmail.com>
2019-11-23 22:39:24 -0600

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,11 @@
+import keys from '../../keys'
+
+export default {
+    apiKey: keys.firebaseKey,
+    authDomain: "vue-employee-manager-dbcd2.firebaseapp.com",
+    databaseURL: "https://vue-employee-manager-dbcd2.firebaseio.com",
+    projectId: "vue-employee-manager-dbcd2",
+    storageBucket: "vue-employee-manager-dbcd2.appspot.com",
+    messagingSenderId: "281688972758",
+    appId: "1:281688972758:web:a5134c0088581c8e3a9877"
+};
